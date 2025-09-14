fc7d26685c964599de661035a162f4b6ed6a5a90
yaser-alazm <yaser.azem@gmail.com>
2025-09-14 16:22:40 +0200

    setup react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next'
 import {Geist, Geist_Mono} from 'next/font/google'
 import './globals.css'
 import {AuthProvider} from '@/context/AuthContext'
+import ReactQueryProvider from '@/providers/ReactQueryProvider'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <ReactQueryProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   )
